1109f03662c323472b272482ff5e44aea340a9ad
Iamankitsarkar <ankitneil@yahoo.com>
2016-06-14 14:09:30 -0400

    functional changes

--- Game.js
+++ Game.js
@@ -32,21 +32,22 @@ MonkeyDefense.Game.prototype = {
      buildEnemies: function() {
         this.enemiesgroup = this.add.group();
         for(var i=0; i<this.totalenemies; i++) {
-            var r = this.enemiesgroup.create(this.rnd.integerInRange(0 + this.world.width), this.rnd.realInRange(this.world.width + 1000, 0), 'enemy', 'Enemies0000');
+            var r = this.enemiesgroup.create(this.rnd.integerInRange(this.world.width, 0 - this.world.height), this.rnd.realInRange(this.world.width + 250, 0 - this.world.width), 'ImageAssets/Evilmonkey.png');
             var scale = this.rnd.realInRange(1.0, 1.0);
-            r.scale.x = scale;
-            r.scale.y = scale;
+           // r.scale.x = scale;
+           // r.scale.y = scale;
             this.physics.enable(r, Phaser.Physics.ARCADE);
             r.enableBody = true;
-            r.body.velocity.y = this.rnd.integerInRange(50, 75);
-            r.animations.add('Fall');
-            r.animations.play('Fall', 24, true);
-            r.checkWorldBounds = true;
-            r.events.onOutOfBounds.add(this.resetRock, this);
+            r.body.velocity.x = this.rnd.integerInRange(-300, -500);
+           // r.animations.add('Fall');
+            //r.animations.play('Fall', 24, true);
+            //r.checkWorldBounds = true;
+            //r.events.onOutOfBounds.add(this.resetEnemy, this);
         }
     
     },	
     
+   
     buildHero: function() {
         
         
