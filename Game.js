52de0d61313374de8b2ff0f66b423619833dc4b8
Iamankitsarkar <ankitneil@yahoo.com>
2016-06-14 14:47:36 -0400

    messing around with spawn enemies

--- Game.js
+++ Game.js
@@ -18,41 +18,35 @@ MonkeyDefense.Game.prototype = {
     buildWorld: function() {
         this.add.image(0, 0, 'background');
         this.buildEnemies();
-        this.buildHero();
+        //this.buildHero();
         
         
     },
     
     update: function () {
      
- },
+    },
 
     
     
      buildEnemies: function() {
         this.enemiesgroup = this.add.group();
         for(var i=0; i<this.totalenemies; i++) {
-            var r = this.enemiesgroup.create(this.rnd.integerInRange(this.world.width, 0 - this.world.height), this.rnd.realInRange(this.world.width + 250, 0 - this.world.width), 'ImageAssets/Evilmonkey.png');
+            var r = this.enemiesgroup.create(this.rnd.integerInRange(this.world.width, this.world.width + this.world.width), this.rnd.realInRange(0, 900), 'enemy', 'Enemy0000');
             var scale = this.rnd.realInRange(1.0, 1.0);
-           // r.scale.x = scale;
-           // r.scale.y = scale;
+            r.scale.x = scale;
+            r.scale.y = scale;
             this.physics.enable(r, Phaser.Physics.ARCADE);
             r.enableBody = true;
             r.body.velocity.x = this.rnd.integerInRange(-300, -500);
-           // r.animations.add('Fall');
+            //r.animations.add('Fall');
             //r.animations.play('Fall', 24, true);
             //r.checkWorldBounds = true;
-            //r.events.onOutOfBounds.add(this.resetEnemy, this);
+            //.events.onOutOfBounds.add(this.resetEnemy, this);
         }
     
-    },	
+    }	
     
-   
-    buildHero: function() {
-        
-        
-        
-    },
      
         
 }  
